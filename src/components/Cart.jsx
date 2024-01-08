58329e40e11bdea2ae88b62da028fc031ef105bd
Alex Wittwer <146585357+alexwittwer@users.noreply.github.com>
2024-01-08 13:37:21 -0500

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,12 +3,11 @@ import { useContext } from "react"
 
 export default function Cart() {
   const cart = useContext(CartContents);
-  const test = cart.forEach((ent) => JSON.stringify(ent));
  
   return (
     <>
-      <p>This cart is empty</p>
-      <p>{test}</p>
+      <p>{cart.map((e) => 
+         return (<p>{JSON.stringjfy(e)}</p>)
     </>
   );
 }
