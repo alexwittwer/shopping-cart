f7f82e9f435439b532be46bd100d4c238085d2b4
Alex Wittwer <146585357+alexwittwer@users.noreply.github.com>
2024-01-08 12:21:34 -0500

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,13 @@
+import { CartContents } from "./App"
+import { useContext } from "react"
+
 export default function Cart() {
+  const { contents } = useContext(CartContents)
+ 
   return (
     <>
       <p>This cart is empty</p>
+      {contents}
     </>
   );
 }
