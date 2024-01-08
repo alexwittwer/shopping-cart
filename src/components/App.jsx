1492a87bf49a13ea61fb3077ec780ef0b17d8644
Alex Wittwer <146585357+alexwittwer@users.noreply.github.com>
2024-01-08 11:57:32 -0500

    add context to app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,31 @@
+import { useState, createContext } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 
+const CartContents = createContext(null);
+const CartAdd = createContext(null);
+const CartDel = createContext(null);
+
 function App() {
+  const [cart, setCart] = useState({});
+  
+
+  function addToCart(game) {
+   return setCart(...cart, game)
+  }
+
+
   return (
+  <CartContents.Provider value={cart} >
+   <CartAdd.Provider value = {addToCart} >
     <div className="flex flex-col justify-between min-h-screen">
       <Header />
       <Outlet />
       <Footer />
     </div>
+   </CartAdd.Provider>
+  </CartContents.Provider>
   );
 }
 
