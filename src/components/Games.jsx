85a74b3fef223d9322474ca77b3a46cddaf59e62
alexwittwer <wittweral@gmail.com>
2024-01-08 19:26:29 -0500

    add more search types

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -4,11 +4,13 @@ export default function Game({ game }) {
   return (
     <div className="cards text-center flex flex-col items-center" key={game.id}>
       <Link
-        className="flex flex-col items-center"
+        className="flex flex-col items-center w-full"
         to={`/games/${game.id}`}
         state={{ game }}
       >
-        <img src={game.background_image} alt="" className=" object-cover" />
+        <div className="card-image w-full">
+          <img src={game.background_image} alt="" className="card-image w-full object-cover" />
+        </div>
         <p className=" bg-slate-700 bg-opacity-50">{game.name}</p>
       </Link>
     </div>
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,49 +1,102 @@
-import { url } from "../utils/api";
-import { useEffect, useState, useContext} from "react";
+import { popular, recent, upcoming } from "../utils/api";
+import { useEffect, useState, useContext } from "react";
 import Games from "./Games";
-import { CartAdd } from "./App"
+import { CartAdd } from "./App";
 
 export default function Shop() {
   const [games, setGames] = useState([]);
   const [loading, setLoading] = useState(true);
   const addToCart = useContext(CartAdd);
-  
+
+  async function fetchData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    setLoading(false);
+
+    return data;
+  }
 
   useEffect(() => {
-    async function fetchData() {
-      const response = await fetch(url);
-      const data = await response.json();
-
-      return data.results;
-    }
-
-    fetchData()
-      .then((results) => setGames(results))
+    fetchData(popular)
+      .then((results) => {
+        setGames(results);
+      })
       .catch((error) => console.warn(error))
       .finally(setLoading(false));
   }, []);
-
 
   if (loading) {
     return <div className="circle-spin-2 self-center place-self-center"></div>;
   }
 
   return (
-    <>
-      <div className="grid xl:grid-cols-4 sm:grid-cols-2 gap-3 my-5 place-items-center">
-        {games.map((game) => {
-          return (
-            <Card key={game.id}>
-              <Games game={game} />
-              <div className="flex justify-between items-center gap-3 m-2">
-                <p>${(Math.round(game.rating * 1000)/100 + 0.09).toFixed(2)}</p>
-                <button onClick={() => addToCart(game)}>Add to cart</button>
-              </div>
-            </Card>
-          );
-        })}
+    <main>
+      <div className="flex gap-3 justify-center">
+        <button
+          onClick={() => {
+            setLoading(true)
+            fetchData(recent).then((results) => setGames(results));
+          }}
+        >
+          Recent
+        </button>
+        <button
+          onClick={() => {
+            setLoading(true)
+            fetchData(popular).then((results) => setGames(results));
+          }}
+        >
+          Popular
+        </button>
+        <button
+          onClick={() => {
+            setLoading(true)
+            fetchData(upcoming).then((results) => setGames(results));
+          }}
+        >
+          Upcoming
+        </button>
       </div>
-    </>
+      <div className="grid xl:grid-cols-4 sm:grid-cols-2 gap-3 my-5 place-items-center">
+        {games.results &&
+          games.results.map((game) => {
+            return (
+              <Card key={game.id}>
+                <Games game={game} />
+                <div className="flex justify-between items-center gap-3 m-2">
+                  <p>
+                    ${(Math.round(game.rating * 1000) / 100 + 0.09).toFixed(2)}
+                  </p>
+                  <button onClick={() => addToCart(game)}>Add to cart</button>
+                </div>
+              </Card>
+            );
+          })}
+      </div>
+      <nav className="flex justify-center gap-5 my-3">
+        <button
+          onClick={() => {
+            if (games.previous) {
+              setLoading(true);
+              fetchData(games.previous).then((results) => setGames(results));
+            }
+          }}
+        >
+          Previous
+        </button>
+        <button
+          onClick={() => {
+            if (games.next) {
+              setLoading(true);
+              fetchData(games.next).then((results) => setGames(results));
+            }
+          }}
+        >
+          Next
+        </button>
+      </nav>
+    </main>
   );
 }
 
