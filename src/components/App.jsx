0c7bfb77366c0b54ed597ea857e5e7021960052a
alexwittwer <wittweral@gmail.com>
2024-01-08 19:29:31 -0500

    set landing page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
 import { useState, createContext } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useParams } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import Landing from "./Landing"
 
 export const CartContents = createContext(null);
 export const CartAdd = createContext(null);
 export const CartDel = createContext(null);
 
 function App() {
+  const name = useLocation();
   const [cart, setCart] = useState([]);
 
   function addToCart(game) {
@@ -19,12 +21,15 @@ function App() {
     setCart(newCart);
   }
 
+  console.log(name);
+
   return (
     <CartContents.Provider value={cart}>
       <CartAdd.Provider value={addToCart}>
         <CartDel.Provider value={delGame}>
           <div className="flex flex-col justify-between min-h-screen">
             <Header />
+            {name.pathname === "/" ? <Landing /> : ""}
             <Outlet />
             <Footer />
           </div>
