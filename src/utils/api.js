c9219417e96d5f0941e4b3724f53ed5c7d46cf66
alexwittwer <wittweral@gmail.com>
2024-01-11 16:07:41 -0500

    couldn't hide the api key

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@ const day = String(date.getDate()).padStart(2, "0");
 const today = `${year}-${month}-${day}`;
 const lastYear = `${year - 1}-${month}-${day}`;
 
-const API_KEY = process.env.API_URL;
+const API_KEY = "122f438994ad4f20891d56632a5002b3";
 const ACCESS = `https://api.rawg.io/api/games?dates=${lastYear},2023-12-31&ordering=-added`;
 const ACCESS2 =
   "https://api.rawg.io/api/games?dates=2023-11-01,2024-01-30&platforms=18,1,7";
